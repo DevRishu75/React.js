8376a984295e96bd84425d8e122146d2f92068df
DevopsRishu <devopsrishabh77@gmail.com>
2025-12-05 22:37:59 +0530

    React-Components

--- React-Components/src/App.jsx
+++ React-Components/src/App.jsx
@@ -1,13 +1,13 @@
-
 import React from 'react'
-
+import card from './components/card.jsx'
 const App = () => {
   return (
     <div>
-      
+    
+      {card()}
+      {card()}
     </div>
   )
 }
 
 export default App
-
---
+++ React-Components/src/components/card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const card = () => {
+    const User = "Rishabh"
+    const age = "7258"
+  return (
+    <div>
+      <div className="card">
+        <h1>Rishabh Upadhayay</h1>
+        <h3>I am {User} and I am {age} years old.</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, laborum?</p>
+      </div>
+    </div>
+  )
+}
+
+export default card
